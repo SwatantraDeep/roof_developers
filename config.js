b4ed86481fa6fb9157a3bd1424d1af8b5f6aceac
SwatantraDeep <s.dangarh999@gmail.com>
2025-10-05 18:57:47 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// Editable config for easy customization
+window.SiteConfig = {
+  companyName: "Roof Developers",
+  tagline: "Creating Roof for All Your Needs",
+  phone: "+91 99999 99999",
+  email: "hello@roofdevelopers.example",
+  location: "Mumbai, India",
+  themeColor: "#0f5671"
+};
